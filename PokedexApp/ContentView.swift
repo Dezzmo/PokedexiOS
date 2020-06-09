e1ac15eba6ec9377c4e91bafbf1beef24e99016c
Dezzmo <45710749+Dezzmo@users.noreply.github.com>
2020-06-09 21:28:52 +0200

    Initial Commit

---
+++ PokedexApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PokedexApp
+//
+//  Created by Sejat Hakimi on 09.06.20.
+//  Copyright © 2020 Sejat Hakimi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
